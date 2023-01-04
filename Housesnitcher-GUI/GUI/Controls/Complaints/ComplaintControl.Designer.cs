3e4e5b5ccec772d52d7e873e10d9703e8ab62b79
daki4 <yordanmitev03@protonmail.com>
2023-01-04 17:25:50 +0200

    feat: Complaints control

---
+++ Housesnitcher-GUI/GUI/Controls/Complaints/ComplaintControl.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Housesnitcher_GUI.GUI.Controls
+{
+    partial class ComplaintControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblDateReference = new System.Windows.Forms.Label();
+            this.rtbDescription = new System.Windows.Forms.RichTextBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(3, 11);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(85, 15);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "ComplaintTitle";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(158, 11);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(87, 15);
+            this.lblDate.TabIndex = 1;
+            this.lblDate.Text = "ComplaintDate";
+            // 
+            // lblDateReference
+            // 
+            this.lblDateReference.AutoSize = true;
+            this.lblDateReference.Location = new System.Drawing.Point(158, 37);
+            this.lblDateReference.Name = "lblDateReference";
+            this.lblDateReference.Size = new System.Drawing.Size(139, 15);
+            this.lblDateReference.TabIndex = 2;
+            this.lblDateReference.Text = "ComplaintDateReference";
+            // 
+            // rtbDescription
+            // 
+            this.rtbDescription.BackColor = System.Drawing.SystemColors.Control;
+            this.rtbDescription.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtbDescription.Location = new System.Drawing.Point(0, 55);
+            this.rtbDescription.Name = "rtbDescription";
+            this.rtbDescription.Size = new System.Drawing.Size(300, 96);
+            this.rtbDescription.TabIndex = 3;
+            this.rtbDescription.Text = "ComplaintRtbDescription";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(192, 170);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(95, 15);
+            this.lblStatus.TabIndex = 4;
+            this.lblStatus.Text = "ComplaintStatus";
+            // 
+            // ComplaintControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.rtbDescription);
+            this.Controls.Add(this.lblDateReference);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "ComplaintControl";
+            this.Size = new System.Drawing.Size(300, 200);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lblTitle;
+        private Label lblDate;
+        private Label lblDateReference;
+        private RichTextBox rtbDescription;
+        private Label lblStatus;
+    }
+}
---
+++ Housesnitcher-GUI/GUI/Controls/Complaints/ComplaintControl.cs
@@ -0,0 +1,10 @@
+﻿namespace Housesnitcher_GUI.GUI.Controls
+{
+    public partial class ComplaintControl : UserControl
+    {
+        public ComplaintControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
