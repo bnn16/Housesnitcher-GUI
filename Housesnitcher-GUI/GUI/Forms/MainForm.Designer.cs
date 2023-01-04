fe51ad14dd5698204e3aa57ee175025414477792
daki4 <yordanmitev03@protonmail.com>
2023-01-04 17:27:03 +0200

    feat: basic window addded to main form

---
+++ Housesnitcher-GUI/GUI/Forms/MainForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Housesnitcher_GUI.GUI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.HomeTabControl = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.HomeTabControl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // HomeTabControl
+            // 
+            this.HomeTabControl.Controls.Add(this.tabPage1);
+            this.HomeTabControl.Controls.Add(this.tabPage2);
+            this.HomeTabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.HomeTabControl.Location = new System.Drawing.Point(0, 0);
+            this.HomeTabControl.Name = "HomeTabControl";
+            this.HomeTabControl.SelectedIndex = 0;
+            this.HomeTabControl.Size = new System.Drawing.Size(1013, 601);
+            this.HomeTabControl.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1005, 573);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 72);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1013, 601);
+            this.Controls.Add(this.HomeTabControl);
+            this.Name = "MainForm";
+            this.Text = "HouseSnitcher";
+            this.HomeTabControl.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private TabControl HomeTabControl;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+    }
+}
