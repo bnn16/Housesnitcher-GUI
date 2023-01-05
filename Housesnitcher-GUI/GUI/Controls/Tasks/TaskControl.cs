a56621778a1924a41732dee88d84c351fa219804
daki4 <yordanmitev03@protonmail.com>
2023-01-05 10:17:44 +0200

    feat: TaskControl functionality

--- Housesnitcher-GUI/GUI/Controls/Tasks/TaskControl.cs
+++ Housesnitcher-GUI/GUI/Controls/Tasks/TaskControl.cs
@@ -1,10 +1,80 @@
-﻿namespace Housesnitcher_GUI.GUI.Controls
+﻿using Housesnitcher_GUI.Models;
+
+namespace Housesnitcher_GUI.GUI.Controls
 {
     public partial class TaskControl : UserControl
     {
-        public TaskControl()
+        public DateTime Date
+        {
+            get
+            {
+                return DateTime.Parse(lblDate.Text);
+            }
+        }
+        public DateTime DateReference
+        {
+            get
+            {
+                return DateTime.Parse(lblDateReference.Text);
+            }
+        }
+        public TennantTaskStatus Status
+        {
+            get
+            {
+                return Enum.Parse<TennantTaskStatus>(lblStatus.Text);
+            }
+            set
+            {
+                lblStatus.Text = value.ToString();
+            }
+        }
+        public string Title
+        {
+            get
+            {
+                return lblTitle.Text;
+            }
+        }
+        public string Description
+        {
+            get
+            {
+                return rtbDescription.Text;
+            }
+        }
+        public TennantTask UpdateUsingObject
+        {
+            set
+            {
+                lblDate.Text = value.DateCreated.ToString();
+                lblDateReference.Text = value.DateDue.ToString();
+                lblStatus.Text = value.Status.ToString();
+                lblTitle.Text = value.Title;
+                rtbDescription.Text = value.Description;
+                PaintColor(value.Status);
+            }
+        }
+
+        public TaskControl(TennantTask task)
         {
             InitializeComponent();
+            lblDate.Text = task.DateCreated.ToString();
+            lblDateReference.Text = task.DateDue.ToString();
+            lblStatus.Text = task.Status.ToString();
+            lblTitle.Text = task.Title;
+            rtbDescription.Text = task.Description;
+            PaintColor(task.Status);
+        }
+        private void PaintColor(TennantTaskStatus status)
+        {
+            lblStatus.ForeColor = status switch
+            {
+                TennantTaskStatus.Failed => Color.Red,
+                TennantTaskStatus.Assigned => Color.Black,
+                TennantTaskStatus.Completed => Color.Green,
+                _ => Color.Aqua,
+            };
         }
     }
 }
