516420fe49987ac1c822c4d62a1ffa079f6167ec
daki4 <yordanmitev03@protonmail.com>
2023-01-07 16:13:20 +0200

    fix: code cleanup

--- Housesnitcher-GUI/GUI/Controls/Complaints/PersonalUserComplaintControl.cs
+++ Housesnitcher-GUI/GUI/Controls/Complaints/PersonalUserComplaintControl.cs
@@ -1,23 +1,17 @@
 ﻿using Housesnitcher_GUI.Models;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace Housesnitcher_GUI.GUI.Controls.Complaints
 {
     public partial class PersonalUserComplaintControl : UserControl
     {
-        public Complaint UpdateUsingObject { set
+        public Complaint UpdateUsingObject
+        {
+            set
             {
                 ComplaintControlView.UpdateUsingObject = value;
                 rtbFeedback.Text = value.ComplaintFeedback ?? "No feedback received Yet.";
-            } }
+            }
+        }
         public PersonalUserComplaintControl()
         {
             InitializeComponent();
