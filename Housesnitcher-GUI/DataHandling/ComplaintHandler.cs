6ba5040e664a2d24be380a86d816aafa6877f04d
Bogdan Nikolov <84232456+bnn16@users.noreply.github.com>
2023-01-12 01:33:15 -0800

    Connection to DB Lodge Complaint
    Creating a complaint to the DB working

--- Housesnitcher-GUI/DataHandling/ComplaintHandler.cs
+++ Housesnitcher-GUI/DataHandling/ComplaintHandler.cs
@@ -1,18 +1,70 @@
 ﻿using Housesnitcher_GUI.DataStorageAbstractions;
 using Housesnitcher_GUI.Models;
+using System.Data.SqlClient;
 
 namespace Housesnitcher_GUI.DataHandling
 {
+
+
     public static class ComplaintHandler
     {
+        private static readonly string _connection = "data source = MSI\\SQLEXPRESS; database=Group Project; integrated security=True";
+
         public static Complaint? LodgeComplaint(Complaint complaint)
         {
-            // membership check, we dont like duplicates
-            if (ComplaintStore.Complaints.Contains(complaint))
+            using (SqlConnection connection = new SqlConnection(_connection))
             {
-                return null;
+                connection.Open();
+                using (SqlCommand command = new SqlCommand("select count(*) from complaints where title like @title and complaint_description like @comp_desc and username like @username and complaint_type like @complaint_type", connection))
+                {
+                    command.Parameters.AddWithValue("@title", complaint.Title);
+                    command.Parameters.AddWithValue("@comp_desc", complaint.Description);
+                    command.Parameters.AddWithValue("@username", complaint.Username);
+                    command.Parameters.AddWithValue("@complaint_type", complaint.Type);
+                    SqlDataReader reader = command.ExecuteReader(); ;
+                    if (reader.HasRows)
+                    {
+                        MessageBox.Show("Complaint Already Exists");
+                        return null;
+
+                    }
+                    reader.Close();
+                    reader.DisposeAsync();
+
+                }
+                connection.Close();
             }
-            ComplaintStore.Complaints.Add(complaint);
+
+
+            try
+            {
+                string query = "Insert into complaints (title, complaint_description, username, complaint_type, date_happened, date_created) values (@title, @complaint_descr, @username, @complaint_type, @date_happened, @date_created)";
+                using (SqlConnection connection = new SqlConnection(_connection))
+                {
+
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@title", complaint.Title);
+                        command.Parameters.AddWithValue("@complaint_descr", complaint.Description);
+                        command.Parameters.AddWithValue("@username", complaint.Username);
+                        command.Parameters.AddWithValue("@complaint_type", complaint.Type);
+                        command.Parameters.AddWithValue("@date_happened", complaint.DateHappened);
+                        command.Parameters.AddWithValue("@date_created", complaint.DateCreated);
+                        command.ExecuteNonQuery();
+
+                    }
+                    connection.Close();
+                }
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+            //    // membership check, we dont like duplicates
+            //    if (ComplaintStore.Complaints.Contains(complaint))
+            //    {
+            //        return null;
+            //    }
+            //ComplaintStore.Complaints.Add(complaint);
             return complaint;
         }
         public static Complaint? AcknowledgeComplaint(Complaint complaint)
@@ -57,6 +109,7 @@ namespace Housesnitcher_GUI.DataHandling
 
         public static List<Complaint> AllComplaints()
         {
+            //https://stackoverflow.com/questions/16856687/getting-data-from-sql-and-putting-in-a-list
             return ComplaintStore.Complaints;
         }
 
