05f6dd2fd664cddb41f430230092c058abd3390a
daki4 <yordanmitev03@protonmail.com>
2023-01-17 01:10:28 +0100

    feat: rework and fixes of `CreateTaskForm`

--- Housesnitcher-GUI/GUI/Forms/CreateTaskForm.cs
+++ Housesnitcher-GUI/GUI/Forms/CreateTaskForm.cs
@@ -1,78 +1,44 @@
-﻿using Housesnitcher_GUI.Models;
+﻿using Housesnitcher_GUI.DataHandling;
+using Housesnitcher_GUI.Models;
 using Housesnitcher_GUI.StateManagement;
 using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace Housesnitcher_GUI.GUI.Forms
 {
     public partial class CreateTaskForm : Form
     {
-        private static readonly string _connection = "data source = MSI\\SQLEXPRESS; database=Group Project; integrated security=True";
-
         public CreateTaskForm()
         {
             InitializeComponent();
         }
-
-
         private void CreateTaskForm_Load(object sender, EventArgs e)
         {
-            foreach (var task in State.complaintTypes)
+            foreach (var type in State.complaintTypes)
             {
-                cmbTasks.Items.Add(task);
+                cmbTasks.Items.Add(type);
+                cmbTasks.AutoCompleteCustomSource.Add(type);
             }
 
-            List<string> usernames = new List<string>();
-
-            using (SqlConnection connection = new SqlConnection(_connection))
-            {
-                connection.Open();
-
-                using (SqlCommand command = new SqlCommand("SELECT username FROM users", connection))
-                {
-                    using (SqlDataReader reader = command.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            usernames.Add(reader["username"].ToString());
-                        }
-                    }
-                }
-            }
-
-            foreach (var username in usernames)
+            foreach (var username in UserHandler.UsernamesList())
             {
                 cmbUsers.Items.Add(username);
+                cmbUsers.AutoCompleteCustomSource.Add(username);
+
             }
         }
-
         private void btnAddTask_Click(object sender, EventArgs e)
         {
-            TennantTask newTask = new TennantTask(txtBoxTaskTitle.Text, txtBoxTaskDescription.Text, cmbUsers.Text, cmbTasks.Text, null, dateTimePicker1.Value);
+            TennantTask newTask = new(txtBoxTaskTitle.Text, txtBoxTaskDescription.Text, cmbUsers.Text, cmbTasks.Text, dateTimePicker1.Value);
 
-            try
+            var res = TennantTaskHandler.AssignTask(newTask);
+            if (res == null)
             {
-                using (SqlConnection connection = new SqlConnection(_connection))
-                {
-                    connection.Open();
-
-                    using (SqlCommand command = new SqlCommand("INSERT INTO tasks (title, task_description, username, task_type, task_status, dateDue, dateCreated) VALUES (@title, @description, @username, @type, @status, @dateDue, @dateCreated)", connection))
-                    {
-                        command.Parameters.AddWithValue("@title", newTask.Title);
-                        command.Parameters.AddWithValue("@description", newTask.Description);
-                        command.Parameters.AddWithValue("@username", newTask.Username);
-                        command.Parameters.AddWithValue("@type", newTask.Type);
-                        command.Parameters.AddWithValue("@status", newTask.Status);
-                        command.Parameters.AddWithValue("@dateDue", newTask.DateDue);
-                        command.Parameters.AddWithValue("@dateCreated", newTask.DateCreated);
-
-                        command.ExecuteNonQuery();
-                    }
-                }
+                MessageBox.Show("Task not created", "Error");
             }
-            catch (Exception ex) { MessageBox.Show(ex.Message); }
-            finally
+            else
             {
-                MessageBox.Show("Task Created!", "Success");
+                MessageBox.Show("Task created", "Success");
             }
         }
     }
