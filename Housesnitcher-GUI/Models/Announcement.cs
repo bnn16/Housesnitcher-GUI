e7578044f5d0f27ffdc4ea75863be9eb5e53bb0a
daki4 <yordanmitev03@protonmail.com>
2023-01-19 03:48:11 +0100

    feat: `Announcement` Model

---
+++ Housesnitcher-GUI/Models/Announcement.cs
@@ -0,0 +1,31 @@
+﻿namespace Housesnitcher_GUI.Models
+{
+    public class Announcement
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Announcer { get; set; }
+        public DateTime Announced { get; set; }
+        public DateTime ReferredDate { get; set; }
+
+        public Announcement(string title, string description, string announcer, DateTime referredDate)
+        {
+            Title = title;
+            Description = description;
+            Announcer = announcer;
+            Announced = DateTime.Now.Date;
+            ReferredDate = referredDate;
+        }
+        public Announcement(string title, string description, string announcer, DateTime announced, DateTime referredDate)
+        {
+            Title = title;
+            Description = description;
+            Announcer = announcer;
+            Announced = announced;
+            ReferredDate = referredDate;
+
+        }
+
+        public Announcement() { }
+    }
+}
