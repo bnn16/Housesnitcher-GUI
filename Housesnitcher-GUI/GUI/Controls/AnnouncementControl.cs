8a0d539c73da9d41aba6ee2c1fcf4c5b74446f7f
daki4 <yordanmitev03@protonmail.com>
2023-01-19 03:53:21 +0100

    feat: `AnnouncementControl`

---
+++ Housesnitcher-GUI/GUI/Controls/AnnouncementControl.cs
@@ -0,0 +1,31 @@
+﻿using Housesnitcher_GUI.Models;
+
+namespace Housesnitcher_GUI.GUI.Controls
+{
+    public partial class AnnouncementControl : UserControl
+    {
+        Announcement _announcement;
+        public Announcement StoredAnnouncement
+        {
+            get { return _announcement; }
+        }
+
+        public Announcement UpdateUsingObject
+        {
+            set
+            {
+                _announcement = value;
+                lblTitle.Text = value.Title;
+                lblAnnounced.Text = value.Announced.ToString();
+                lblDateReferenced.Text = value.ReferredDate.ToString();
+                lblAnnouncer.Text = value.Announcer;
+                tbDescription.Text = value.Description;
+            }
+        }
+        public AnnouncementControl(Announcement announcement)
+        {
+            InitializeComponent();
+            UpdateUsingObject = announcement;
+        }
+    }
+}
