7cae2cf2a0c632f97e93af3b11b08a6802d88f7c
daki4 <yordanmitev03@protonmail.com>
2023-01-19 03:53:12 +0100

    feat: `CreateAnnouncement` form

---
+++ Housesnitcher-GUI/GUI/Forms/CreateAnnouncement.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Housesnitcher_GUI.GUI.Forms
+{
+    partial class CreateAnnouncement
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbDescription = new System.Windows.Forms.TextBox();
+            this.tbTitle = new System.Windows.Forms.TextBox();
+            this.dtpDateReferenced = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbDescription
+            // 
+            this.tbDescription.Location = new System.Drawing.Point(145, 59);
+            this.tbDescription.Multiline = true;
+            this.tbDescription.Name = "tbDescription";
+            this.tbDescription.Size = new System.Drawing.Size(271, 59);
+            this.tbDescription.TabIndex = 1;
+            // 
+            // tbTitle
+            // 
+            this.tbTitle.Location = new System.Drawing.Point(145, 30);
+            this.tbTitle.Name = "tbTitle";
+            this.tbTitle.Size = new System.Drawing.Size(100, 23);
+            this.tbTitle.TabIndex = 2;
+            // 
+            // dtpDateReferenced
+            // 
+            this.dtpDateReferenced.Location = new System.Drawing.Point(145, 124);
+            this.dtpDateReferenced.Name = "dtpDateReferenced";
+            this.dtpDateReferenced.Size = new System.Drawing.Size(200, 23);
+            this.dtpDateReferenced.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(107, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Title:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Description:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Date happening:";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(341, 162);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 8;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(260, 162);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 9;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // CreateAnnouncement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 272);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtpDateReferenced);
+            this.Controls.Add(this.tbTitle);
+            this.Controls.Add(this.tbDescription);
+            this.Name = "CreateAnnouncement";
+            this.Text = "CreateAnnouncement";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox tbDescription;
+        private TextBox tbTitle;
+        private DateTimePicker dtpDateReferenced;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button btnCreate;
+        private Button btnCancel;
+    }
+}
---
+++ Housesnitcher-GUI/GUI/Forms/CreateAnnouncement.cs
@@ -0,0 +1,32 @@
+﻿using Housesnitcher_GUI.DataHandling;
+using Housesnitcher_GUI.GUI.Events;
+using Housesnitcher_GUI.Models;
+
+namespace Housesnitcher_GUI.GUI.Forms
+{
+    public partial class CreateAnnouncement : Form
+    {
+        User _user;
+        public CreateAnnouncement(User user)
+        {
+            _user = user;
+            InitializeComponent();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            AnnouncementHandler.CreateAnnouncement(new Announcement(
+                tbTitle.Text,
+                tbDescription.Text,
+                _user.Username,
+                dtpDateReferenced.Value.Date));
+            ForceRefreshEvent.RaiseRefreshEvent(this, new EventArgs());
+            Hide();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
