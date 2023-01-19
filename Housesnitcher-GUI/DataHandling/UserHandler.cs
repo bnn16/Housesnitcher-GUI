da51345311ef598f069c4202c62ab3f9f4582616
daki4 <yordanmitev03@protonmail.com>
2023-01-19 03:48:48 +0100

    feat: `AnnouncementControl`

--- Housesnitcher-GUI/DataHandling/UserHandler.cs
+++ Housesnitcher-GUI/DataHandling/UserHandler.cs
@@ -5,31 +5,30 @@ namespace Housesnitcher_GUI.DataHandling
 {
     public static class UserHandler
     {
-        private static readonly string _connection = $"data source = MSI\\SQLEXPRESS; database=Group Project; integrated security=SSPI";
+        private static readonly string _connection = $"data source = {StateManagement.State.DataSource}; database={StateManagement.State.ConnectionString}; integrated security=SSPI";
         public static User? CreateUser(string username, string unhashedPassword)
         {
             var user = new User(username, unhashedPassword);
-            return _createUser(user.Username, user.Password);
+            return _createUser(user);
         }
         public static User? CreateUser(User user)
         {
-            return _createUser(user.Username, user.Password);
+            return _createUser(user);
         }
-        private static User? _createUser(string username, string password)
+        private static User? _createUser(User user)
         {
             using (SqlConnection connection = new SqlConnection(_connection))
             {
                 connection.Open();
-                using (SqlCommand command = new SqlCommand("select count(*) from users where username = @username and pass = @pass", connection))
+                using (SqlCommand command = new SqlCommand("select count(*) from users where username = @username", connection))
                 {
-                    command.Parameters.AddWithValue("@username", username);
-                    command.Parameters.AddWithValue("@pass", password);
+                    command.Parameters.AddWithValue("@username", user.Username);
+                    command.Parameters.AddWithValue("@pass", user.Password);
 
                     int UserExist = (int)command.ExecuteScalar();
 
                     if (UserExist > 0)
                     {
-                        MessageBox.Show("User Already Exists");
                         return null;
 
                     }
@@ -39,7 +38,7 @@ namespace Housesnitcher_GUI.DataHandling
 
             try
             {
-                string query = "Insert into users (username, pass) values (@username, @pass)";
+                string query = "Insert into users (username, pass, permission) values (@username, @pass, @perm)";
                 using (SqlConnection connection = new SqlConnection(_connection))
                 {
 
@@ -47,20 +46,16 @@ namespace Housesnitcher_GUI.DataHandling
 
                     using (SqlCommand command = new SqlCommand(query, connection))
                     {
-                        command.Parameters.AddWithValue("@username", username);
-                        command.Parameters.AddWithValue("@pass", password);
+                        command.Parameters.AddWithValue("@username", user.Username);
+                        command.Parameters.AddWithValue("@pass", user.Password);
+                        command.Parameters.AddWithValue("@perm", user.AuthLevel);
                         command.ExecuteNonQuery();
 
                     }
                     connection.Close();
                 }
             }
-            catch (Exception ex) { MessageBox.Show(ex.Message); }
-            var user = new User(username, password);
-            if (username == "Admin" && password == "admin")
-            {
-                user.AuthLevel = ScopeLevel.Admin;
-            }
+            catch (Exception) { return null; }
             return user;
         }
 
@@ -89,26 +84,21 @@ namespace Housesnitcher_GUI.DataHandling
             {
                 string query = "Select * from users where username = @username and pass = @pass";
 
-                using (SqlConnection connection = new SqlConnection(_connection))
+                using SqlConnection connection = new SqlConnection(_connection);
+                connection.Open();
+                using SqlCommand command = new SqlCommand(query, connection);
+
+                command.Parameters.AddWithValue("@username", username);
+                command.Parameters.AddWithValue("@pass", password);
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
                 {
-                    connection.Open();
-                    using (SqlCommand command = new SqlCommand(query, connection))
-                    {
-
-                        command.Parameters.AddWithValue("@username", username);
-                        command.Parameters.AddWithValue("@pass", password);
-                        SqlDataReader reader = command.ExecuteReader();
-                        if (reader.HasRows)
-                        {
-                            var user = new User(username, password);
-                            return user;
-                        }
-                        else
-                        {
-                            return null;
-                        }
-                    }
-
+                    var user = new User(reader["username"].ToString(), reader["password"].ToString(), Convert.ToInt32(reader["permission"].ToString()));
+                    return user;
+                }
+                else
+                {
+                    return null;
                 }
             }
             catch (Exception ex) { MessageBox.Show(ex.Message); }
